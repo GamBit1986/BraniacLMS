ee88790907744b60478c7fd6179573441bfcfcac
GamBit1986 <mazepa86@gmail.com>
2023-01-19 19:44:40 +0300

    lesson_5 Authapp done

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+rom django.urls import path
 
 from mainapp import views
 from mainapp.apps import MainappConfig
@@ -17,5 +17,4 @@ urlpatterns = [
     ),
     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
-    path("login/", views.LoginPageView.as_view(), name="login"),
 ]
