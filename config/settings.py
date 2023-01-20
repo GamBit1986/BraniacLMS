72536c3c7389c4192e9e37fba1bb525d752232b6
GamBit1986 <52270313+GamBit1986@users.noreply.github.com>
2023-01-20 12:28:32 +0300

    Lesson 5 (#3)
    * lesson_5 GitHub is done

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "markdownify.apps.MarkdownifyConfig",
-
     "social_django",
     "mainapp",
     "authapp",
